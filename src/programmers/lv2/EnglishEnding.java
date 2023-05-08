114e67e97eed9e553bef6d558ac40405279b40d6
somin <angiwon12345@nate.com>
2023-05-08 22:10:00 +0900

    프로그래머스  lv2

--- src/programmers/lv2/EnglishEnding.java
+++ src/programmers/lv2/EnglishEnding.java
@@ -1,24 +1,20 @@
 package programmers.lv2;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class EnglishEnding {
     public int[] solution(int n, String[] str) {
         int[] answer = {0,0};
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add(str[0]);
         for (int i = 1; i < str.length; i++) {
-            if (str[i - 1].charAt(str[i-1].length() - 1) != str[i].charAt(0)) {
+            if (str[i - 1].charAt(str[i-1].length() - 1) != str[i].charAt(0)||hashSet.contains(str[i])) {
                 answer[1] = (int)i / n +1;
                 answer[0] = i % n +1;
                 break;
             }
-            for (int j = 0; j <= i - 1; j++) {
-                if (str[i] == str[j]) {
-                    answer[1] = (int)i / n +1;
-                    answer[0] = i % n +1;
-                    break;
-                }
-            }
-
+            hashSet.add(str[i]);
         }
 
 
@@ -28,8 +24,8 @@ public class EnglishEnding {
 
     public static void main(String[] args) {
         EnglishEnding T = new EnglishEnding();
-        int n = 3;
-        String[] str = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+        int n = 2;
+        String[] str = {"hello", "one", "even", "never", "now", "world", "draw"};
         System.out.println(Arrays.toString(T.solution(n,str)));
 
     }
